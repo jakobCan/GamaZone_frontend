0d21f4b219955aacb783064b847c4e1524855444
PixelGoogs <gulliverstrash@gmail.com>
2022-03-15 20:01:10 +0100

    Created new shop with list/card switch

--- js/shop.js
+++ js/shop.js
@@ -1,83 +1,89 @@
+var colors = ["Crimson ", "Cyan ", "DarkBlue ", "DarkCyan ", "DarkGoldenRod ", "DarkGray ", "DarkGrey ", "DarkGreen ", "DarkKhaki ", "DarkMagenta ", "DarkOliveGreen ", "DarkOrange "];
+
 $(document).ready(function () {
 
-    var table = $('#example').DataTable({
-        'dom':
-            "<'row'<'col-sm-12 col-md-6'l><'col-sm-12 col-md-6'<'float-md-right ml-2'B>f>>" +
-            "<'row'<'col-sm-12'tr>>" +
-            "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
+    var table = $('#register').DataTable({
+        dom: 'fBti',
+        pageLength: -1,
+        buttons: [
+            {
+                text: '<i class="fa fa-id-badge fa-fw fa-lg" aria-hidden="true"></i>',
+                className: 'animated bounce',
+                action: function () {
+
+                    $("#register").toggleClass("cards");
+                    $("#register thead").toggle();
+                    $("#card-toggle .fa").toggleClass("fa-table");
+                    $("#card-toggle .fa").toggleClass("fa-id-badge");
+
+                    if($("#register").hasClass("cards")){
+                        var max = 0;
+                        $('#register tr').each(function() {
+                            max = Math.max($(this).height(), max);
+                        }).height(max);
+                    } else {
+                        $("#register tr").each(function(){
+                            $(this).height("auto");
+                        });
+                    }
+
+                },
+                attr:  {
+                    title: 'Change views',
+                    id: 'card-toggle'
+                }
+            }
+        ],
+        select: 'single',
         "ajax": {
             "url": "http://localhost:8080/products",
             "type": "GET",
             "dataSrc": ""
-        },        'buttons': [ 'csv', {
-            'text': '<i class="fa fa-id-badge fa-fw" aria-hidden="true"></i>',
-            'action': function (e, dt, node) {
+        },
+        columns: [
+            {   /* created column to show a picture just to make this demo look better */
+                orderable: false, data: 'Photo', name: 'Photo', orderable: false, defaultContent: '', title: 'Photo',
+                visible: true, className: 'text-center', width: '20px',
 
-                $(dt.table().node()).toggleClass('cards');
-                $('.fa', node).toggleClass(['fa-table', 'fa-id-badge']);
-
-                dt.draw('page');
-            },
-            'className': 'btn-sm',
-            'attr': {
-                'title': 'Change views',
-            }
-        }],
-        "columns": [
-            { "data": "id" },
-            { "data": "name" },
-            { "data": "price" },
-            { "data": "category" },
-            { "data": "description" },
-            { "data": "tagline" },
-            { "data": 'picture',
-                "render": function (url,type,full){
-                    return '<img height="50%" width="50%" src="../images/Planets/planet-1519089_1280.jpg"/>';
+                createdCell: function (td, cellData, rowData, row, col) {
+                    var $ctl = $('<i class="fa fa-user fa-fw"></i>').css('color', colors[Math.round(Math.random() * colors.length) + 1])
+                    $(td).append($ctl);
                 }
             },
-        ],
-        'drawCallback': function (settings) {
-            var api = this.api();
-            var $table = $(api.table().node());
-
-            if ($table.hasClass('cards')) {
-
-                // Create an array of labels containing all table headers
-                var labels = [];
-                $('thead th', $table).each(function () {
-                    labels.push($(this).text());
-                });
-
-                // Add data-label attribute to each cell
-                $('tbody tr', $table).each(function () {
-                    $(this).find('td').each(function (column) {
-                        $(this).attr('data-label', labels[column]);
-                    });
-                });
-
-                var max = 0;
-                $('tbody tr', $table).each(function () {
-                    max = Math.max($(this).height(), max);
-                }).height(max);
-
-            } else {
-                // Remove data-label attribute from each cell
-                $('tbody td', $table).each(function () {
-                    $(this).removeAttr('data-label');
-                });
-
-                $('tbody tr', $table).each(function () {
-                    $(this).height('auto');
-                });
+            /* I added a label to the column for the field name which will show up in the card display */
+            {
+                data: "name", name: "name",
+                render: function (data, type, row, meta) { return (type === 'display' ? '<label>'+ $($('#register').DataTable().column(meta.col).header()).html() +':</label>' : null ) + (function(data, type, row) { return data +', '+ row.office })(data, type, row, meta); }
+            },
+            {
+                data: "category", name: "category", class: 'text-left',
+                render: function (data, type, row, meta) { return (type === 'display' ? '<label>'+ $($('#register').DataTable().column(meta.col).header()).html() +':</label>' : null ) + data; }
+            },
+            {
+                data: "tagline", name: "tagline",
+                render: function (data, type, row, meta) { return (type === 'display' ? '<label>'+ $($('#register').DataTable().column(meta.col).header()).html() +':</label>' : null ) + data; }
+            },
+            {
+                data: "description", name: "description", class: 'text-right',
+                render: function (data, type, row, meta) { return (type === 'display' ? '<label>'+ $($('#register').DataTable().column(meta.col).header()).html() +':</label>' : null ) + data; }
+            },
+            {
+                data: "price", name: "price",
+                render: function (data, type, row, meta) { return (type === 'display' ? '<label>'+ $($('#register').DataTable().column(meta.col).header()).html() +':</label>' : null ) + data; }
+            },
+            {
+                data: null, name: null, class: 'center',
+                render: function (data, type, row, meta) {return `<button id=${data.id} class='deleteButton'>Buy me!</button>`;
+                }
             }
-        }
+        ]
     })
-
         .on('select', function (e, dt, type, indexes) {
             var rowData = table.rows(indexes).data().toArray()
-            $('#row-data').html(JSON.stringify(rowData));
+            $('#row-data').html(JSON.stringify(rowData))
         })
         .on('deselect', function () {
-            $('#row-data').empty();
+            $('#row-data').html('')
         })
+
 });
